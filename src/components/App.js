45379498a776e0e61a3ad14b2018372197799697
ARAFAT AMAN ALIM <76907565+Arafat-alim@users.noreply.github.com>
2022-08-16 18:47:55 +0530

    code refractoring

--- src/components/App.js
+++ src/components/App.js
@@ -42,13 +42,12 @@ class App extends Component {
     	return(
     		<div id="main">
     			{
-    				//Correct the percentage conditional for correct output
+    				
     				this.state.list.map(function(item,index){
 				   		if(item.percent > 75)
 				   		{
 				   			return(
-				   				//Conditional statement in the className of below div to give callName="bg-pink"
-				   				//if the percent >= 90 else className = "" 
+				   			
 				   				<div key={index} className={item.percent >= 90 ? "bg-pink" : ""}>
 					   				<div className="name">
 					   					 {/* //Write code to display name */}
